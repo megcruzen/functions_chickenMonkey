b781f44f0f4fe5782db8423240911d5a80b21709
Meg Cruzen <megcruzen@gmail.com>
2018-11-30 11:10:18 -0600

    Updated code during review

--- chickenmonkey.js
+++ chickenmonkey.js
@@ -10,18 +10,17 @@ for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
         console.log(currentNumber) // Only 2, 4, 6 will appear
     }
 }
-
 */
 
 for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
-    if (currentNumber % 5 === 0 && currentNumber % 7 !== 0) {
+    if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+        console.log("ChickenMonkey")
+    }
+    else if (currentNumber % 5 === 0) {
         console.log("Chicken")
     }
-    else if (currentNumber % 7 === 0 && currentNumber % 5 !== 0 ) {
+    else if (currentNumber % 7 === 0) {
         console.log("Monkey")
-    }
-    else if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
-        console.log("ChickenMonkey")
     }
     else {
         console.log(currentNumber)
